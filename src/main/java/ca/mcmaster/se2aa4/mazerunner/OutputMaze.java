3837ae45fc4a867b0536497136aecebdb9daa94c
Vanessa Lai <laiv3@mcmaster.ca>
2024-01-21 22:12:40 -0500

    Walking Skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/OutputMaze.java
+++
@@ -1,9 +0,0 @@
-package ca.mcmaster.se2aa4.mazerunner;
-
-public class OutputMaze {
-    public OutputMaze(String[] args){}
-    
-    public String mazeOutput(Maze maze){
-        return " ";
-    } 
-}
